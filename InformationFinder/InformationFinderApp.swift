e3a01cc1114abc3a3c264408716b76ee9e84906e
rhimcc <rio.mccormack25@gmail.com>
2024-08-17 20:28:12 +1000

    Initial Commit

---
+++ InformationFinder/InformationFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  InformationFinderApp.swift
+//  InformationFinder
+//
+//  Created by Rhianna McCormack on 17/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct InformationFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InformationFinderUITests/InformationFinderUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  InformationFinderUITestsLaunchTests.swift
+//  InformationFinderUITests
+//
+//  Created by Rhianna McCormack on 17/8/2024.
+//
+
+import XCTest
+
+final class InformationFinderUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

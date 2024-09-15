b2c1da80f5ab6016bd640dbabbd30c643b5e3fde
rhimcc <rio.mccormack25@gmail.com>
2024-09-15 15:46:09 +1000

    brain search

--- InformationFinder/Views/Brain Bank/BrainSearchResults.swift
+++ InformationFinder/Views/Brain Bank/BrainSearchResults.swift
@@ -15,19 +15,39 @@ struct BrainSearchResults: View {
         if searchViewModel.searchQuery.isEmpty {
             return Array(topics.filter {$0.beenSwiped})
           } else {
-              return topics.filter { $0.topicName.localizedCaseInsensitiveContains(searchViewModel.searchQuery)}
+              return topics.filter { $0.topicName.localizedCaseInsensitiveContains(searchViewModel.searchQuery) && $0.beenSwiped}
           }
       }
     
     var body: some View {
         NavigationStack {
-            ScrollView {
-                ForEach(Array(filteredTopics.enumerated()), id: \.element) { index, topic in
-                    NavigationLink {
-                        TopicDetail(scrollPosition: topic.scrollPosition, topic: topic)
-                    } label : {
-                        TopicRow(topic: topic, colour: index % 2 == 0 ? .midGreen : .lightGreen)
-                            .padding([.top, .leading, .trailing], 20)
+            if (filteredTopics.isEmpty) {
+                VStack (alignment: .center){
+                    Image(systemName: "brain")
+                        .font(.system(size: 70))
+                        .padding(30)
+                    Text("No swiped topics matches your search!")
+                        .bold()
+                        .font(.system(size: 25))
+                        .padding(.bottom, 10)
+                    Text("Search in home to see if it is an available topic, or try another search")
+                        .bold()
+                        .font(.system(size: 15))
+                    
+                }.foregroundStyle(.darkGreen)
+                .multilineTextAlignment(.center)
+                .frame(width: 300, height: 500)
+                .position(x: UIScreen.main.bounds.width/2, y: 300)
+            } else {
+                ScrollView {
+                    ForEach(Array(filteredTopics.enumerated()), id: \.element) { index, topic in
+                        NavigationLink {
+                            TopicDetail(scrollPosition: topic.scrollPosition, topic: topic)
+                        } label : {
+                            TopicRow(topic: topic, colour: index % 2 == 0 ? .midGreen : .lightGreen)
+                                .padding([.top, .leading, .trailing], 20)
+                        }
+                        
                     }
                     
                 }
--- InformationFinder/Views/Home/SearchResultsView.swift
+++ InformationFinder/Views/Home/SearchResultsView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct SearchResultsView: View {
     var body: some View {
-        Text("Hello, World!")
+       Text("yey")
     }
 }
 
